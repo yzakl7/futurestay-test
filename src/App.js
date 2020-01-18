ac05f61ba736462e8a8f186a32ec66e062ad494f
yzakl7 <isaacsauriortegon@gmail.com>
2020-01-18 11:25:06 -0600

    public restaurada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 import './App.scss';
+import Calendar from './components/calendar/Calendar';
 
-export default function App() { 
-
-
-
-class Calendar extends Component {
-  render() {
-    return (
-      <div>
-        123
-      </div>
-    )
-  }
+export default function App() { // quedo en el mismo archivo por practicidad
+  return (
+    <div id="MainContainer">
+      <Calendar />
+    </div>
+  )
 }
